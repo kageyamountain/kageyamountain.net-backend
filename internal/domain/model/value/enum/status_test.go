d72effa966fb1e7f6947968b687761bb3ef6af19
kageyamountain <kageyamountain@users.noreply.github.com>
2025-08-22 02:03:59 +0900

    VOのユニットテストコード実装

---
+++ internal/domain/model/value/enum/status_test.go
@@ -0,0 +1,69 @@
+package enum
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseStatus(t *testing.T) {
+	t.Parallel()
+
+	t.Run("正常系", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name       string
+			input      string
+			wantStatus Status
+		}{
+			{name: "draft", input: "draft", wantStatus: StatusDraft},
+			{name: "publish", input: "publish", wantStatus: StatusPublish},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+				// Arrange
+
+				// Act
+				got, err := ParseStatus(tt.input)
+
+				// Assert
+				a := assert.New(t)
+				a.NoError(err)
+				a.Equal(tt.wantStatus, got)
+			})
+		}
+	})
+
+	t.Run("異常系", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name       string
+			input      string
+			wantStatus Status
+		}{
+			{name: "空文字", input: "", wantStatus: ""},
+			{name: "想定されていない値", input: "UnknownStatus", wantStatus: ""},
+			{name: "大文字小文字の違い", input: "Draft", wantStatus: ""},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+				// Arrange
+
+				// Act
+				got, err := ParseStatus(tt.input)
+
+				// Assert
+				a := assert.New(t)
+				a.Error(err)
+				a.Equal("invalid status: "+tt.input, err.Error())
+				a.Equal(tt.wantStatus, got)
+			})
+		}
+	})
+}
---
+++ internal/domain/model/value/enum/tag_test.go
@@ -0,0 +1,75 @@
+package enum
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseTag(t *testing.T) {
+	t.Parallel()
+
+	t.Run("正常系", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name    string
+			input   string
+			wantTag Tag
+		}{
+			{name: "Go", input: "Go", wantTag: TagGo},
+			{name: "Gin", input: "Gin", wantTag: TagGin},
+			{name: "JavaScript", input: "JavaScript", wantTag: TagJavaScript},
+			{name: "TypeScript", input: "TypeScript", wantTag: TagTypeScript},
+			{name: "Vue.js", input: "Vue.js", wantTag: TagVueJS},
+			{name: "AWS", input: "AWS", wantTag: TagAWS},
+			{name: "DynamoDB", input: "DynamoDB", wantTag: TagDynamoDB},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+				// Arrange
+
+				// Act
+				got, err := ParseTag(tt.input)
+
+				// Assert
+				a := assert.New(t)
+				a.NoError(err)
+				a.Equal(tt.wantTag, got)
+			})
+		}
+	})
+
+	t.Run("異常系", func(t *testing.T) {
+		t.Parallel()
+
+		tests := []struct {
+			name    string
+			input   string
+			wantTag Tag
+		}{
+			{name: "空文字", input: "", wantTag: ""},
+			{name: "想定されていない値", input: "UnknownTag", wantTag: ""},
+			{name: "小文字・大文字の違い", input: "go", wantTag: ""},
+			{name: "ドット無し", input: "Vuejs", wantTag: ""},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.name, func(t *testing.T) {
+				t.Parallel()
+				// Arrange
+
+				// Act
+				got, err := ParseTag(tt.input)
+
+				// Assert
+				a := assert.New(t)
+				a.Error(err)
+				a.Equal(err.Error(), "invalid tag: "+tt.input)
+				a.Equal(tt.wantTag, got)
+			})
+		}
+	})
+}
