983a7ce8b2cab05ae2c7dd054da83a3d42fd3782
kageyamountain <kageyamountain@users.noreply.github.com>
2025-10-25 10:08:59 +0900

    add linter thelper

--- test/helper/initialize.go
+++ test/helper/initialize.go
@@ -11,26 +11,26 @@ import (
 
 var once sync.Once
 
-func InitializeIntegrationTest(t testing.TB) {
-	t.Helper()
+func InitializeIntegrationTest(tb testing.TB) {
+	tb.Helper()
 
 	once.Do(func() {
-		loadEnvFile(t)
+		loadEnvFile(tb)
 	})
 }
 
-func loadEnvFile(t testing.TB) {
-	t.Helper()
+func loadEnvFile(tb testing.TB) {
+	tb.Helper()
 
 	env := os.Getenv("ENV")
 
 	if env == "ci" {
 		// CI環境
 		err := godotenv.Load("../../.env.ci")
-		require.NoError(t, err)
+		require.NoError(tb, err)
 	} else {
 		// ローカル開発環境
 		err := godotenv.Load("../../.env.dev")
-		require.NoError(t, err)
+		require.NoError(tb, err)
 	}
 }
