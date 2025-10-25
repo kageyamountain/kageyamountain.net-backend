d4a4b1159628bf31a3edf9a164d123040af457c0
kageyamountain <kageyamountain@users.noreply.github.com>
2025-10-25 11:57:26 +0900

    contextの出力ができいなかったので修正

--- internal/feature/articles_get/handler.go
+++ internal/feature/articles_get/handler.go
@@ -24,10 +24,9 @@ func (a *ArticlesGetHandler) ArticlesGet(c *gin.Context, params openapi.Articles
 
 	// TODO リクエストパラメータを利用した公開年とタグのフィルタリング機能実装
 
-	// ユースケースの実行
 	useCaseOutput, err := a.useCase.Execute(ctx)
 	if err != nil {
-		slog.Error("failed to ArticlesGet use case", slog.Any("err", err))
+		slog.ErrorContext(ctx, "failed to ArticlesGet use case", slog.Any("err", err))
 		c.AbortWithStatusJSON(http.StatusInternalServerError, openapi.Error{
 			Code:    openapi.InternalServerError,
 			Message: "server error",
